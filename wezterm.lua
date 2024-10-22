1ab3ff4186ffaea421dc647e4d932b9e160d7de6
Akhmad Dias <akhmad.dias94@gmail.com>
2024-10-22 23:48:03 +0500

    initial config

---
+++ wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require("wezterm")
+
+config = wezterm.config_builder()
+
+config = {
+  automatically_reload_config = true,
+  enable_tab_bar = false,
+  window_close_confirmation = "NeverPrompt",
+  window_decorations = "RESIZE",
+  default_cursor_style = "BlinkingBar",
+  color_scheme = "Nord (Gogh)",
+  font = wezterm.font("JetBrains Mono", { weight = "Bold"}),
+  font_size = 12.5
+}
+
+return config
